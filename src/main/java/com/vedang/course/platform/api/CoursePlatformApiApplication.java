4ce2be8822810a20302d77090cefff881cba9f72
Vedang <techvedang17@gmail.com>
2026-01-30 00:31:27 +0530

    spring boot new project setup

---
+++ src/main/java/com/vedang/course/platform/api/CoursePlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.vedang.course.platform.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursePlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vedang/course/platform/api/CoursePlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedang.course.platform.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
