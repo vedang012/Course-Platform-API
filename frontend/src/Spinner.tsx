7661eaefa547492fba465f90b1468a789e5f1706
Vedang <techvedang17@gmail.com>
2026-02-23 19:39:47 +0530

    Delete frontend directory

--- frontend/src/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-export default function Spinner() {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
-    </div>
-  );
-}
